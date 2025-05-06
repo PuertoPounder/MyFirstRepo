cc3b614768f97b9ded04ad316d97ac679e71c798
Andrew P Rodriquez <rodriquezap@gmail.com>
2025-05-06 09:50:01 -0600

    Update MainNasa.jsx
    Updated code to support image and video files from nasas astronomy image of the day. Before it only supported images and nasa posted a video that wasn't supported. So this adds video support.

--- nasa-react-app/src/components/MainNasa.jsx
+++ nasa-react-app/src/components/MainNasa.jsx
@@ -1,8 +1,25 @@
-export default function MainNasa(props) {
-    const { data } = props
+export default function MainNasa({ data }) {
     return (
-        <div className="imgContainer">
-        <img src={data.hdurl} alt={data.title || 'big-img'} className="bgImage"></img>
-        </div>
+      <div className="imgContainer">
+        {data.media_type === 'image' ? (
+          <img
+            src={data.hdurl ?? data.url}
+            alt={data.title}
+            className="bgImage"
+          />
+        ) : data.media_type === 'video' ? (
+          <div className="videoContainer">
+            <iframe
+              title={data.title}
+              src={data.url}
+              frameBorder="0"
+              allow="encrypted-media"
+              allowFullScreen
+              className="bgVideo"
+            />
+          </div>
+        ) : null}
+      </div>
     )
-}
+  }
+  
